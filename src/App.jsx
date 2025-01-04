e087c7e2f67bbdf08a6022de8a555cdd0edb1073
Hama Pashazadeh <pashazad@msu.edu>
2025-01-04 15:57:16 -0500

    implement state management in Square component and update Board to use state for squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 
-function Square({ value }) {
+
+function Square() {
+
+  const [value, setValue] = useState(null);
+
+
   function handleClick() {
-    console.log("clicked!");
+    setValue('X');
   }
 
 
@@ -9,30 +15,32 @@ function Square({ value }) {
     <button 
       className="square"
       onClick={handleClick}
-      >
-        {value}
+    >
+      {value}
     </button>
   );
 }
 
 export default function Board() {
 
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   )
